4d15ab1372c905aab157c9cf15015f33487a87e5
Karthik <karthikchary078@gmail.com>
2025-01-24 22:54:28 +0530

    Update StockTrend_With_News.py

--- pages/StockTrend_With_News.py
+++ pages/StockTrend_With_News.py
@@ -108,9 +108,6 @@ def main():
                     stock_analysis_prompt = f"""
                     You are a stock analysis expert. Given the stock data (such as Open, High, Low, Close, Volume), perform a detailed technical analysis. 
                     Analyze the trends, predict future movements, and incorporate insights using from this news: {latest_article['description']}.
-                    Provide a structured response in the following format:
-                    1. **Trend Analysis**: Describe the overall trend (e.g., upward, downward, sideways)
-                    2. **Impact of News**: Summarize how the latest news may affect the stock.
                     
                     """
 
